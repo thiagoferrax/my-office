1840fd63ff2444eac854bc525ac8c03bf013679f
Thiago Ferraz Vieira da Cunha <thiago.cunha@dataprev.gov.br>
2019-04-05 13:24:06 -0300

    Updating the version of office-map component.

--- frontend/src/common/form/selectWithIcon.jsx
+++ frontend/src/common/form/selectWithIcon.jsx
@@ -5,13 +5,13 @@ import Select from './select'
 
 export default props => (
     <Grid cols={props.cols || 12}>
-        <div class="form-group">
+        <div className="form-group">
             <If test={props.label}>
                 <label>{props.label}</label>
             </If>
-            <div class="input-group">
-                <div class="input-group-addon">
-                    <i class={`fa fa-${props.icon}`}></i>
+            <div className="input-group">
+                <div className="input-group-addon">
+                    <i className={`fa fa-${props.icon}`}></i>
                 </div>
                 <Select {...props} onlyCombo={true}/>
             </div>
